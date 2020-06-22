e4d27616e022ef463205e75292817d02f9109b0e
Gabriel Bouchard <gbouchard@ova.ai>
2020-06-22 19:01:56 -0400

    deleted build

--- cmake-build-debug/dependencies/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Documents/GitHub/PrototypeEngine/dependencies/assimp/contrib/zlib
-# Build directory: C:/Users/User/Documents/GitHub/PrototypeEngine/cmake-build-debug/dependencies/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/dependencies/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Documents/GitHub/PrototypeEngine/dependencies/glm
-# Build directory: C:/Users/User/Documents/GitHub/PrototypeEngine/cmake-build-debug/dependencies/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
