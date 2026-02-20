1d3cd3e40864f92c02409eeb2c30bc00ef303cf0
Gabriel <gaboutch@outlook.com>
2026-02-20 12:52:16 -0500

    Update NodeSystem.h

--- core/scene/systems/NodeSystem.h
+++ core/scene/systems/NodeSystem.h
@@ -13,7 +13,7 @@ namespace HBE {
 		RawVector<entity_handle> root_nodes;
 		bool dirty = false;
 		uint32_t node_count = 0;
-		event_subscription_id attach_subscription_id = 0;
+		event_subscription_id attach_subscription_id ;
 		event_subscription_id detach_subscription_id;
 	public:
 		NodeSystem(Scene *scene);
