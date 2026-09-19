c71e49c2578b147b88643b57a463a0b045b584f8
Gabriel Bouchard <33035583+Goutch@users.noreply.github.com>
2026-09-19 12:23:57 -0400

    Update Profiler.cpp

--- core/utility/Profiler.cpp
+++ core/utility/Profiler.cpp
@@ -59,9 +59,7 @@ namespace HBE {
 		current_node->min = current_node->count > 1 ? std::min(profile_stack.top().node->min, ms) : ms;
 		current_node->max = current_node->count > 1 ? std::max(profile_stack.top().node->max, ms) : ms;
 
-
-		double total = current_node->time * (current_node->count - 1);
-		current_node->time = (total + ms) / current_node->count;
+		current_node->time += ms;
 		std::string indent = "";
 		for (int i = 0; i < profile_stack.size(); ++i) {
 			indent += '-';
@@ -70,17 +68,18 @@ namespace HBE {
 		profile_stack.pop();
 	}
 
-	void printNodeAverange(ProfileGraphNode *node, int indent) {
+	void printNodeAverange(ProfileGraphNode *node, int indent, uint32_t root_count) {
 		std::string indent_str_tabs = "";
 		for (int i = 0; i < indent; ++i) {
 			indent_str_tabs += "|    ";
 		}
 		Log::message(indent_str_tabs + "[" + node->message + "]" + std::to_string(node->count) + "\n" +
-		             indent_str_tabs + "|avg: " + std::to_string(node->time) + "ms|" + "\n" +
-		             indent_str_tabs + "|min: " + std::to_string(node->min) + "ms|#" + std::to_string(node->min_i) + "\n" +
-		             indent_str_tabs + "|max: " + std::to_string(node->max) + "ms|#" + std::to_string(node->max_i));
+					indent_str_tabs + "|frame avg: " + std::to_string(node->time/root_count) + "ms|" + "\n" +
+					indent_str_tabs + "|avg: " + std::to_string(node->time/node->count) + "ms|" + "\n" +
+					indent_str_tabs + "|min: " + std::to_string(node->min) + "ms|#" + std::to_string(node->min_i) + "\n" +
+					indent_str_tabs + "|max: " + std::to_string(node->max) + "ms|#" + std::to_string(node->max_i));
 		for (auto n: node->sub_nodes) {
-			printNodeAverange(n.second, indent + 1);
+			printNodeAverange(n.second, indent + 1, root_count);
 		}
 		delete node;
 	}
@@ -88,8 +87,8 @@ namespace HBE {
 	void Profiler::printAverange() {
 		int indent = 0;
 		for (auto n: nodes) {
-			printNodeAverange(n.second, indent);
+			uint32_t root_count = n.second->count;
+			printNodeAverange(n.second, indent, root_count);
 		}
 	}
 }
-
